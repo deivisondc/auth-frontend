e7e323e68dbad935efa22e5fc12603974312dc15
Deivison Cardoso <deivisondc@gmail.com>
2022-06-03 21:28:44 -0300

    initial commit

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,17 @@
+import { useContext, useEffect } from "react"
+import { AuthContext } from "../contexts/AuthContext"
+import { api } from "../services/api"
+
+export default function Dashboard() {
+  const { user } = useContext(AuthContext)
+
+  useEffect(() => {
+    api.get('/me')
+  }, [])
+
+  return (
+    <div>
+      <h1>Dashboard: {user?.email}</h1>
+    </div>
+  )
+}
